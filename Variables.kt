58ecec14838610e04e55794bef6c5de1c9e350df
Bruno Rijo <bruno_rijo@hotmail.com>
2022-06-28 20:13:00 -0300

    Kotlin Basics

---
+++ Variables.kt
@@ -0,0 +1,18 @@
+/* 
+Create a String variable rainbowColor, set its color value, then change it.
+Create a variable blackColor whose value cannot be changed once assigned. Try changing it anyway.
+*/
+
+fun main() {
+   var rainbowColor = "Red"
+   val blackColor = "Black"
+    
+   rainbowColor = "Orange"
+   rainbowColor = "Yellow"
+   rainbowColor = "Green"
+   rainbowColor = "Light Blue"
+   rainbowColor = "Blue"
+   rainbowColor = "Violet"
+    
+   blackColor = "White" //It's impossible to change a variable seted as val 
+}
