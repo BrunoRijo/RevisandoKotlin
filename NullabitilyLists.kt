1fabc7ab2effa70347b10a4f3008c8cbcf14ff99
Bruno Rijo <bruno_rijo@hotmail.com>
2022-06-28 20:32:27 -0300

    Kotlin Basics

---
+++ NullabitilyLists.kt
@@ -0,0 +1,13 @@
+fun main() {
+   
+    /* Practice Time: Nullability/Lists 
+     * Create a list with two elements that are null; do it in two different ways.
+     * Next, create a list where the list is null.*/
+    
+    val list: List<String?> = listOf(null, null)
+    
+    var secondList : List<String?> = ArrayList()
+    secondList.plus(null)
+    secondList.plus(null)
+    
+}
