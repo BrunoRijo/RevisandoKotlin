cd6116be7f523f059a6ebe51596c8cf921d6b34c
Bruno Rijo <bruno_rijo@hotmail.com>
2022-06-28 20:49:19 -0300

    Kotlin Basics

---
+++ StringTemplate.kt
@@ -0,0 +1,12 @@
+fun main() {
+   
+    /* Create three String variables for trout, haddock and snapper.
+     * Use a String template to print ehater you do or don't like to eat these kinds of fish */ 
+    
+    val trout = "Trout"
+    val haddock = "haddock"
+    val snapper = "snapper"
+    
+    println("I like to eat ${trout}, ${haddock} and ${snapper}.")
+    
+}
