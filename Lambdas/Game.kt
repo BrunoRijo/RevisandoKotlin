b4c9d11c2d8ead967832f078308ab838e48df840
Bruno Rijo <bruno_rijo@hotmail.com>
2022-07-17 14:58:17 -0300

    Adding HighOrderFunctions

--- Lambdas/Game.kt
+++ Lambdas/Game.kt
@@ -6,14 +6,21 @@ class Game {
     var south = path.add(Directions.SOUTH)
     var east = path.add(Directions.EAST)
     var west = path.add(Directions.WEST)
-    var end  = end()
+    var end  = {
+        path.add(Directions.END);
+        println("Game Over: ");
+        print(path);
+        path.clear();
+        false }
 
-    fun end(){
-        path.add(Directions.END)
-        println("Game Over: ")
-        print(path)
-        path.clear()
-        false
+    fun move(where: () -> Boolean) = where.invoke()
+
+    fun makeMove(argument : String?) {
+        if (argument.equals("n")) move({ north })
+        else if (argument.equals("s")) move({ south })
+        else if (argument.equals("e")) move({ east })
+        else if (argument.equals("w")) move({ west })
+        else move(end)
     }
 
 }
