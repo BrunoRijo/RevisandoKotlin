b8997c22deb15d59e965dd0322f7f8ff6c0115a0
Bruno Rijo <bruno_rijo@hotmail.com>
2022-07-17 14:59:22 -0300

    Adding HighOrderFunctions

--- Lambdas/Main.kt
+++ Lambdas/Main.kt
@@ -2,9 +2,16 @@ package Lambdas
 
 fun main() {
     var game = Game()
+    /*
     game.north
     game.south
     game.east
     game.west
     game.end
+    */
+    
+    while (true){
+        print("Enter a direction: n/s/e/w:")
+        game.makeMove(readLine())
+    }
 }
